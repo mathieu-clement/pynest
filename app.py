49f92088211b6e9de9dbf809c48eefea8235cbf1
Mathieu Clement <mclement2@dons.usfca.edu>
2017-11-04 21:55:54 -0700

    Replace NestApi with Nest and api with nest

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import os.path
 import requests
 import sys
 
-class NestApi:
+CLIENT_ID = '797003a0-272b-462d-a739-ee365674be34'
+CLIENT_SECRET = '03Cwjo7Tiy7hieo0AWbAvxnTb'
+
+class Nest:
     def __init__(self, code=None, access_token=None):
         if code:
             logging.debug('Init using code ' + code)
@@ -18,10 +21,11 @@ class NestApi:
         else:
             raise Exception('Must provide either code or access token')
 
+
     def get_access_token(self, code):
         url = "https://api.home.nest.com/oauth2/access_token"
-        payload = (("client_id", '797003a0-272b-462d-a739-ee365674be34'),
-                   ('client_secret', '03Cwjo7Tiy7hieo0AWbAvxnTb'),
+        payload = (("client_id", CLIENT_ID),
+                   ('client_secret', CLIENT_SECRET),
                    ('grant_type', 'authorization_code'),
                    ('code', code))
         #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -33,12 +37,14 @@ class NestApi:
             raise Exception('Could not authenticate')
         return json['access_token']
 
+
     def get_thermostat_ids(self):
         r = self.request('GET', 'https://developer-api.nest.com/devices/thermostats/')
         return list(r.json().keys())
-    
+
+
     def get_thermostat(self):
-        thermostat_ids = api.get_thermostat_ids()
+        thermostat_ids = nest.get_thermostat_ids()
         if len(thermostat_ids) > 1:
             raise Exception("More than one thermostat on this account.")
         thermostat_id = thermostat_ids[0]
@@ -57,23 +63,27 @@ class NestApi:
             logging.debug('status code: ' + str(r.status_code))
         return r
 
+
 class Thermostat:
-    def __init__(self, api, device_id):
-        self.api = api
+    def __init__(self, nest, device_id):
+        self.nest = nest
         self.device_id = device_id
         self.url = 'https://developer-api.nest.com/devices/thermostats/' + device_id
 
+
     @property
     def ambient_temperature(self):
-        return float(self.api.request('GET', self.url + '/ambient_temperature_c').text)
+        return float(self.nest.request('GET', self.url + '/ambient_temperature_c').text)
+
 
     @property
     def target_temperature(self):
-        return float(self.api.request('GET', self.url + '/target_temperature_c').text)
+        return float(self.nest.request('GET', self.url + '/target_temperature_c').text)
+
 
     @target_temperature.setter
     def target_temperature(self, value):
-        r = self.api.request('PUT', self.url, json={'target_temperature_c': value})
+        r = self.nest.request('PUT', self.url, json={'target_temperature_c': value})
         if r.status_code != 200:
             raise Exception(r.text)
 
@@ -84,13 +94,16 @@ class ConfigFile:
     def __init__(self, filename='config.json'):
         self.filename = filename
 
+
     def read(self):
         with open(self.filename) as data_file:
             return json.load(data_file)
 
+
     def write(self, data):
         with open(self.filename, 'w') as data_file:
             json.dump(data, data_file)
+
 
     def exists(self):
         return os.path.exists(self.filename)
@@ -103,19 +116,19 @@ if __name__ == '__main__':
         config = cfg_file.read()
 
 
-    api = None
+    nest = None
     if len(sys.argv) > 1:
-        api = NestApi(access_token=sys.argv[1])
+        nest = Nest(access_token=sys.argv[1])
     elif config and 'access_token' in config:
-        api = NestApi(access_token=config['access_token'])
+        nest = Nest(access_token=config['access_token'])
     else:
-        import webbrowser; webbrowser.open('https://home.nest.com/login/oauth2?client_id=797003a0-272b-462d-a739-ee365674be34&state=STATE', new=2)
+        import webbrowser; webbrowser.open('https://home.nest.com/login/oauth2?client_id=' + CLIENT_ID + '&state=STATE', new=2)
         code = input("Enter code: ")
-        api = NestApi(code=code)
-        config = {'access_token': api.access_token}
+        nest = Nest(code=code)
+        config = {'access_token': nest.access_token}
         cfg_file.write(config)
 
-    thermostat = api.get_thermostat()
+    thermostat = nest.get_thermostat()
     print('Ambient temperature:', thermostat.ambient_temperature)
 
     t = thermostat.target_temperature
